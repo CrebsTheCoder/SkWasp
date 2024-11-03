6f597351d674b16bdf69862ed28ffcadddeb02f8
Crebs <72847555+CrebsTheCoder@users.noreply.github.com>
2024-11-03 15:01:13 +0800

    Update ExprWorldBorderWarningTime.java

--- src/main/java/com/shanebeestudios/skbee/elements/worldborder/expressions/ExprWorldBorderWarningTime.java
+++ src/main/java/com/shanebeestudios/skbee/elements/worldborder/expressions/ExprWorldBorderWarningTime.java
@@ -42,7 +42,7 @@ public class ExprWorldBorderWarningTime extends SimplePropertyExpression<WorldBo
         Timespan timespan = (Timespan) delta[0];
         if (timespan == null) return;
 
-        int seconds = (int)(timespan.getTicks() / 20);
+        int seconds = (int)(timespan.getAs(Timespan.TimePeriod.TICK) / 20);
         for (WorldBorder border : getExpr().getArray(event)) {
             border.setWarningTime(seconds);
         }
